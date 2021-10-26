8f0c8f00b99141636aa607c25c80ce51b0f7a253
E-Sahra-B <93211429+E-Sahra-B@users.noreply.github.com>
2021-10-26 19:59:09 +0300

    deneme eklendi

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -37,7 +37,7 @@ namespace UI
             services.AddSession();
             services.AddDistributedMemoryCache();
         }
-
+        // Denemeeeee
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
